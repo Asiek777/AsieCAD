6968b99094ea1920046f002bc8237d8e96dc9ac8
Asiek777 <joachimkowalewski@wp.pl>
2020-04-27 13:30:54 +0200

    Correct cubic spline distance dependent interpolation

--- AsieCAD/cubicInterpolated.cpp
+++ AsieCAD/cubicInterpolated.cpp
@@ -1,5 +1,7 @@
 #include "cubicInterpolated.h"
-
+#include <iosfwd>
+#include <vector>
+#include <glm/detail/type_mat.hpp>
 #include "BezierC0.h"
 
 
@@ -20,47 +22,59 @@ void CubicInterpolated::Render()
 	clearExpired();
 	if (points.size() == 0)
 		return;
-	int knotCount = points.size();
-	std::vector<glm::vec3> knots(knotCount);
-	for (int i = 0; i < knotCount; i++)
-		knots[i] = points[i].point.lock()->GetCenter();
-	std::vector<glm::vec3> a(knotCount - 1), b(knotCount - 1), c(knotCount - 1), d(knotCount - 1);
-	std::vector<glm::vec3> alfa(knotCount), beta(knotCount), diag(knotCount), D(knotCount);
-	std::vector<glm::vec3> bezierPoints(3 * knotCount - 2);
-	for (int i = 0; i < knotCount; i++) {
-		alfa[i] = glm::vec3(1);
-		beta[i] = glm::vec3(1);
-		diag[i] = glm::vec3(4);
-		D[i] = 3.f * (knots[i == knotCount - 1 ? knotCount - 1 : i + 1] - knots[i == 0 ? i : i - 1]);
-	}
-	diag[0] = diag[knotCount - 1] = glm::vec3(2);
-
-	//solving equation
-	beta[0] /= diag[0];
-	D[0] /= diag[0];
-	for (int i = 1; i < knotCount; i++) {
-		glm::vec3 m = 1.0f / (diag[i] - alfa[i] * beta[i - 1]);
-		beta[i] *= m;
-		D[i] = (D[i] - alfa[i] * D[i - 1]) * m;
-	}
-	for (int i = knotCount - 2; i > 0; i--)
-		D[i] -= beta[i] * D[i + 1];	
-	D[0] -= beta[0] * D[1];
-	
-	for (int i = 0; i < knotCount - 1; i++) {
-		a[i] = knots[i];
-		b[i] = D[i];
-		c[i] = 3.f * (knots[i + 1] - knots[i]) - 2.f * D[i] - D[i + 1];
-		d[i] = 2.f * (knots[i] - knots[i + 1]) + D[i] + D[i + 1];
-		bezierPoints[3 * i] = knots[i];
-		bezierPoints[3 * i + 1] = a[i] + 1.f / 3.f * b[i];
-		bezierPoints[3 * i + 2] = a[i] + 2.f / 3.f * b[i] + 1.f / 3.f * c[i];
-	}
-	bezierPoints[3 * knotCount - 3] = knots[knotCount - 1];
+	std::vector<glm::vec3> bezierPoints = CalcBezierPoints();
 
 	glm::vec3 color = isSelected ? glm::vec3(1, 0, 0) : glm::vec3(1);
 	BezierC0::DrawBezierCurve(bezierPoints, color);
 	if (drawBroken)
 		BezierC0::DrawBroken(bezierPoints);
+	RenderSelectedPoints();
+}
 
+std::vector<glm::vec3> CubicInterpolated::CalcBezierPoints()
+{
+	int knotCount = points.size();
+	std::vector<glm::vec3> knots(knotCount);
+	for (int i = 0; i < knotCount; i++)
+		knots[i] = points[i].point.lock()->GetCenter();
+	std::vector<glm::vec3> a(knotCount - 1), b(knotCount - 1), c(knotCount), d(knotCount - 1);
+	std::vector<float> alfa(knotCount - 1), beta(knotCount - 1), diag(knotCount - 1),
+	                   dist(knotCount - 1);
+	std::vector<glm::vec3> bezierPoints(3 * knotCount - 2);
+	for (int i = 0; i < knotCount - 1; i++)
+		dist[i] = glm::distance(knots[i], knots[i + 1]);
+	for (int i = 1; i < knotCount - 1; i++) {
+		alfa[i] = dist[i - 1] / (dist[i - 1] + dist[i]);
+		beta[i] = dist[i] / (dist[i - 1] + dist[i]);
+		diag[i] = 2;
+		c[i] = 3.f * ((knots[i + 1] - knots[i]) / dist[i] - 
+			(knots[i] - knots[i - 1]) / dist[i - 1]) / (dist[i - 1] + dist[i]);
+	}
+
+	//solving equation
+	beta[1] /= diag[1];
+	c[1] /= diag[1];
+	for (int i = 2; i < knotCount - 1; i++) {
+		float m = 1.0f / (diag[i] - alfa[i] * beta[i - 1]);
+		beta[i] *= m;
+		c[i] = (c[i] - alfa[i] * c[i - 1]) * m;
+	}
+	for (int i = knotCount - 3; i > 1; i--)
+		c[i] -= beta[i] * c[i + 1];	
+	c[1] -= beta[1] * c[2];
+	c[0] = c[knotCount - 1] = glm::vec3(0);
+	
+	for (int i = 0; i < knotCount - 1; i++) {
+		a[i] = knots[i];
+		d[i] = (c[i + 1] - c[i]) / (3.f * dist[i]);
+		c[i] *= dist[i] * dist[i];
+		d[i] *= dist[i] * dist[i] * dist[i];
+		b[i] = knots[i + 1] - a[i] - c[i] - d[i];
+		
+		bezierPoints[3 * i] = knots[i];
+		bezierPoints[3 * i + 1] = a[i] + 1.f / 3.f * b[i];
+		bezierPoints[3 * i + 2] = a[i] + 2.f / 3.f * b[i] + 1.f / 3.f * c[i];
+	}
+	bezierPoints[3 * knotCount - 3] = knots[knotCount - 1];
+	return bezierPoints;
 }
--- AsieCAD/cubicInterpolated.h
+++ AsieCAD/cubicInterpolated.h
@@ -4,6 +4,7 @@ class CubicInterpolated :
 	public Curve
 {
 	static int Number;
+	std::vector<glm::vec3> CalcBezierPoints();
 public:
 	CubicInterpolated();
 	void Render() override;	
